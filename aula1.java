22f685268577185db56ee9f0a8599890647838bd
Lívia Duarte <74926169+liviaduart@users.noreply.github.com>
2021-05-17 18:59:25 -0300

    Create aula1.java

---
+++ aula1.java
@@ -0,0 +1,32 @@
+/**
+ * Escreva uma descrição da classe aula1 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class aula1
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * Construtor para objetos da classe aula1
+     */
+    public aula1()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int sampleMethod(int y)
+    {
+        // aula1
+        return x + y;
+    }
+}
